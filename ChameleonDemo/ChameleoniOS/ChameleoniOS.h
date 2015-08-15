670c3d979d660395eca7daf3b0ee74f513a093d3
Ewan Mellor <ewan@tipbit.com>
2015-08-15 01:18:44 -0700

    Added a ChameleoniOS framework target.

---
+++ ChameleonDemo/ChameleoniOS/ChameleoniOS.h
@@ -0,0 +1,19 @@
+//
+//  ChameleoniOS.h
+//  ChameleoniOS
+//
+//  Created by Ewan Mellor on 8/15/15.
+//  Copyright (c) 2015 Vicc Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChameleoniOS.
+FOUNDATION_EXPORT double ChameleoniOSVersionNumber;
+
+//! Project version string for ChameleoniOS.
+FOUNDATION_EXPORT const unsigned char ChameleoniOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChameleoniOS/PublicHeader.h>
+
+#import <ChameleoniOS/Chameleon.h>
